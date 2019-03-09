c48fda178aacb697d08a014b4f7aa00f18efdd13
Joko Irianto <irianto94@outlook.com>
2019-03-09 14:48:48 +0700

    Combining Testify with Table-Driven Tests

--- testify/main.go
+++ testify/main.go
@@ -1,15 +1,114 @@
 package main
 
 import (
-    "fmt"
+	"fmt"
 )
 
 // Addtional between 2 variable
-func Additional(x int, y int)(result int) {
-    result = x + y
-    return result
+func Additional(x int, y int) (result int) {
+	result = x + y
+	return result
+}
+
+// Subtraction between 2 variabel (first - second)
+func Subtraction(x int, y int) (result int) {
+	result = x - y
+	return result
+}
+
+// angkaToRomawi function to convert int to romawi number
+func AngkaToRomawi(i int) string {
+	romawi := ""
+
+	for i > 0 {
+		if i >= 5000 {
+			romawi += "IƆƆ"
+			i -= 5000
+			continue
+		}
+
+		if i >= 1000 {
+			romawi += "M"
+			i -= 1000
+			continue
+		}
+
+		if i >= 900 {
+			romawi += "CM"
+			i -= 900
+			continue
+		}
+
+		if i >= 500 {
+			romawi += "D"
+			i -= 500
+			continue
+		}
+
+		if i >= 400 {
+			romawi += "CD"
+			i -= 400
+			continue
+		}
+
+		if i >= 100 {
+			romawi += "C"
+			i -= 100
+			continue
+		}
+
+		if i >= 90 {
+			romawi += "XC"
+			i -= 90
+			continue
+		}
+
+		if i >= 50 {
+			romawi += "L"
+			i -= 50
+			continue
+		}
+
+		if i >= 40 {
+			romawi += "XL"
+			i -= 40
+			continue
+		}
+
+		if i >= 10 {
+			romawi += "X"
+			i -= 10
+			continue
+		}
+
+		if i >= 9 {
+			romawi += "IX"
+			i -= 9
+			continue
+		}
+
+		if i >= 5 {
+			romawi += "V"
+			i -= 5
+			continue
+		}
+
+		if i >= 4 {
+			romawi += "IV"
+			i -= 4
+			continue
+		}
+
+		if i >= 1 {
+			romawi += "I"
+			i -= 1
+			continue
+		}
+	}
+
+	return romawi
 }
 
 func main() {
-    fmt.Println("Hello World")
+	fmt.Println("Hello World")
 }
--- testify/main_test.go
+++ testify/main_test.go
@@ -1,19 +1,61 @@
-// tradiontal testing
-package main 
+package main
 
 import (
-    "testing"
-    "github.com/stretchr/testify/assert"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
 )
 
-// TestAdditional
+// TestAdditional with tradional way
 func TestAdditional(t *testing.T) {
-    if Additional(3, 7) != 10 {
-        t.Error("Expected 3 + 7 to equal 10")
-    }
+	if Additional(3, 7) != 10 {
+		t.Error("Expected 3 + 7 to equal 10")
+	}
+}
+
+// TestSubtraction with tradional way
+func TestSubtraction(t *testing.T) {
+	if Subtraction(21, 6) != 15 {
+		t.Error("Expected 21 - 6 to equal 15")
+	}
 }
 
 // TestAdditional with testify
 func TestAdditionalWithTestify(t *testing.T) {
-    assert.Equal(t, Additional(10,5), 15)
+	assert.Equal(t, Additional(10, 5), 15)
+}
+
+// TestAdditional with testify
+func TestSubtractionWithTestify(t *testing.T) {
+	assert.Equal(t, Subtraction(20, 4), 16)
+}
+
+// TestAngkaToRomawi with testify
+func TestAngkaToRomawi(t *testing.T) {
+	var tests = []struct {
+		input    int
+		expected string
+	}{
+		{1, "I"},
+		{3, "III"},
+		{4, "IV"},
+		{5, "V"},
+		{6, "VI"},
+		{10, "X"},
+		{11, "XI"},
+		{12, "XII"},
+		{13, "XIII"},
+		{14, "XIV"},
+		{15, "XV"},
+		{19, "XIX"},
+		{20, "XX"},
+		{30, "XXX"},
+		{1637, "MDCXXXVII"},
+	}
+
+	assert := assert.New(t)
+
+	for _, test := range tests {
+		assert.Equal(AngkaToRomawi(test.input), test.expected)
+	}
 }
